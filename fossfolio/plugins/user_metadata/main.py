c7494ef753210e1bcbb634ea94620a9f99bea7f0
Anuran Roy <anuranroy02@gmail.com>
2022-07-07 23:29:21 +0530

    Changed hardcoded value to system hostname

--- fossfolio/plugins/user_metadata/main.py
+++ fossfolio/plugins/user_metadata/main.py
@@ -5,10 +5,8 @@ The run() method MAY or MAY NOT receive any arguments or keyword arguments.
 The run() method must return an object ( a string or a dictionary) from which value(s) will be injected during building.
 """
 
+import socket
 
 def run(template_str, *args, **kwargs):
-    user = "Anuran"
-
-    new_string: str = template_str.replace(r"<% user %>", user)
-
-    return new_string
+    user = socket.gethostname()
+    return template_str.replace(r"<% user %>", user)
